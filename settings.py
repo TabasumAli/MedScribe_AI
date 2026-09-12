f7dffdd66fad97b8f48edb392be9ac3cb8494d6a
TabasumAli <tabasumshahjillani@gmail.com>
2026-09-12 19:39:15 +0500

    remove commented code from settings file

--- settings.py
+++ settings.py
@@ -4,6 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-# LLM_MODEL = "openai/gpt-oss-120b"
 LLM_MODEL = "openai/gpt-oss-120b"
 SUPPORTED_LANGUAGES = ["English", "Urdu"]
